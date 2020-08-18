e812942a0e5af877cf6c7a9131bbf4f8e2f31795
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-08-18 17:38:03 -0400

    custom types post and user complete

--- graphql-basics_v2/src/index.js
+++ graphql-basics_v2/src/index.js
@@ -1,18 +1,28 @@
 import { GraphQLServer } from 'graphql-yoga';
+
 // String, Boolean, Int, Float, ID --> 5 Scalar Types i.e. stores a single value
 // Type Definitions (schema)
 const typeDefs = `
   type Query {
     me: User!
+    post: Post!
   }
 
   type User {
-    id: ID!
-    name: String!
-    email: String!
+    id: ID!,
+    name: String!,
+    email: String!,
     age: Int
   }
+
+  type Post {
+    id: ID!,
+    title: String!,
+    body: String!,
+    published: Boolean!
+  }
 `;
+
 // Resolvers
 const resolvers = {
   Query: {
@@ -24,8 +34,17 @@ const resolvers = {
         age: 28,
       };
     },
+    post() {
+      return {
+        id: '123abc',
+        title: 'First Post',
+        body: 'First post body',
+        published: true,
+      };
+    },
   },
 };
+
 const server = new GraphQLServer({ typeDefs, resolvers });
 server.start(() => {
   console.log('GraphQL server is up');
