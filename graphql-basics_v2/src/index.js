c721a28334213890cda680657f64bee7c7df27c8
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-08-18 17:12:14 -0400

    graphql initial setup

--- graphql-basics_v2/src/index.js
+++ graphql-basics_v2/src/index.js
@@ -1,1 +1,19 @@
-console.log('hello graph ql');
+import { GraphQLServer } from 'graphql-yoga';
+// Type Definitions (schema)
+const typeDefs = `
+  type Query {
+    hello: String!
+  }
+`;
+// Resolvers
+const resolvers = {
+  Query: {
+    hello() {
+      return 'This is my first query';
+    },
+  },
+};
+const server = new GraphQLServer({ typeDefs, resolvers });
+server.start(() => {
+  console.log('GraphQL server is up');
+});
