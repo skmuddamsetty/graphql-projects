dfe20d54e45e066bad2de44616f6b4dfc06ec458
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-08-19 09:41:36 -0400

    seperated typedefs to schema file

--- graphql-basics_v2/src/index.js
+++ graphql-basics_v2/src/index.js
@@ -72,71 +72,6 @@ let comments = [
   },
 ];
 
-// String, Boolean, Int, Float, ID --> 5 Scalar Types i.e. stores a single value
-// Type Definitions (schema)
-const typeDefs = `
-  type Query {
-    users(query: String): [User!]!,
-    posts(query: String): [Post!]!,
-    comments: [Comment!]!
-    me: User!,
-    post: Post!
-  }
-
-  type Mutation {
-    createUser(data: CreateUserInput!): User!,
-    deleteUser(id: ID!): User!,
-    createPost(data: CreatePostInput!): Post!,
-    deletePost(id: ID!): Post!
-    createComment(data: CreateCommentInput!): Comment!,
-    deleteComment(id: ID!): Comment!
-  }
-
-  type User {
-    id: ID!,
-    name: String!,
-    email: String!,
-    age: Int,
-    posts: [Post!]!,
-    comments: [Comment!]!
-  }
-
-  type Post {
-    id: ID!,
-    title: String!,
-    body: String!,
-    published: Boolean!,
-    author: User!,
-    comments: [Comment!]!
-  }
-
-  type Comment {
-    id: ID!,
-    text: String!,
-    author: User!
-    post: Post!
-  }
-
-  input CreateUserInput {
-    name: String!,
-    email: String!
-    age: Int
-  }
-
-  input CreatePostInput {
-    title: String!,
-    body: String!,
-    published: Boolean!
-    author: ID!
-  }
-
-  input CreateCommentInput {
-    text: String!,
-    author: ID!,
-    post: ID!
-  }
-`;
-
 // Resolvers
 const resolvers = {
   Query: {
@@ -319,7 +254,10 @@ const resolvers = {
   },
 };
 
-const server = new GraphQLServer({ typeDefs, resolvers });
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers,
+});
 server.start(() => {
   console.log('GraphQL server is up');
 });
