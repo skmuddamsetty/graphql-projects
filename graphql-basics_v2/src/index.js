8a16a4345c0be23b6ed3008a1704c329c6981309
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-08-18 22:24:04 -0400

    users and posts filtering data using params

--- graphql-basics_v2/src/index.js
+++ graphql-basics_v2/src/index.js
@@ -1,9 +1,59 @@
 import { GraphQLServer } from 'graphql-yoga';
 
+// Demo Users data
+const users = [
+  {
+    id: 1,
+    name: 'Santhosh',
+    email: 'santhosh@example.com',
+    age: 30,
+  },
+  {
+    id: 2,
+    name: 'Sunitha',
+    email: 'sunitha@example.com',
+    age: 28,
+  },
+  {
+    id: 3,
+    name: 'Lakhi',
+    email: 'lakhi@example.com',
+    age: 2,
+  },
+];
+
+const posts = [
+  {
+    id: 1,
+    title: 'First Post',
+    body: 'First Post Body',
+    published: true,
+  },
+  {
+    id: 2,
+    title: 'Second Post',
+    body: 'Second Post Body',
+    published: true,
+  },
+  {
+    id: 3,
+    title: 'Third Post',
+    body: 'Third Post Body',
+    published: false,
+  },
+  {
+    id: 4,
+    title: 'Fourth Post',
+    body: 'Fourth Post Body',
+    published: false,
+  },
+];
 // String, Boolean, Int, Float, ID --> 5 Scalar Types i.e. stores a single value
 // Type Definitions (schema)
 const typeDefs = `
   type Query {
+    users(query: String): [User!]!
+    posts(query: String): [Post!]!
     me: User!,
     post: Post!
   }
@@ -41,6 +91,24 @@ const resolvers = {
         body: 'First post body',
         published: true,
       };
+    },
+    users(parent, { query = '' }, ctx, info) {
+      if (query.length === 0) {
+        return users;
+      }
+      return users.filter((user) =>
+        user.name.toLowerCase().includes(query.toLowerCase())
+      );
+    },
+    posts(parent, { query = '' }, ctx, info) {
+      if (query.length === 0) {
+        return posts;
+      }
+      return posts.filter(
+        (post) =>
+          post.title.toLowerCase().includes(query.toLowerCase()) ||
+          post.body.toLowerCase().includes(query.toLowerCase())
+      );
     },
   },
 };
