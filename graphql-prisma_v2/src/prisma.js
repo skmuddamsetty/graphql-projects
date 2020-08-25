8e7aac0b085e3dcd482377fa4e163aeca8fed3eb
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-08-25 08:41:18 -0400

    lock down prisma to outsid world

--- graphql-prisma_v2/src/prisma.js
+++ graphql-prisma_v2/src/prisma.js
@@ -2,6 +2,7 @@ import { Prisma } from 'prisma-binding';
 const prisma = new Prisma({
   typeDefs: 'src/generated/prisma.graphql',
   endpoint: 'http://localhost:4466',
+  secret: 'thisismysupersecrettext',
 });
 export { prisma as default };
 
